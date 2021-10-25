cf3a1c2c4c5451dcd4e0252a02add9c79766fcb9
Rahul Rajesh <53638652+rahullrajesh@users.noreply.github.com>
2021-10-25 20:53:57 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    nav: true,
+    dots: true,
+    items: 3,
+    navText: ['<span></span>', '<span></span>'],
+    autoplay: false
+  })
+});
